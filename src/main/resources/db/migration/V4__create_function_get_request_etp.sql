8cd660afae0bc53f0566384545584fdde5124bcb
Abbos Akramov <abbosakramov121@gmail.com>
2022-06-28 12:38:30 +0500

    V4__get_request_etp.sql migration was added

---
+++ src/main/resources/db/migration/V4__create_function_get_request_etp.sql
@@ -0,0 +1,38 @@
+create or replace function
+    get_request_etp(v_lot_id bigint, v_from_doc_date DATE, v_to_doc_date DATE,
+                    v_organ_name character varying, v_from_sum_lot bigint, v_to_sum_lot bigint,
+                    v_month integer, v_state integer, v_pltf integer) returns text
+    language plpgsql
+as
+$$
+Declare
+    r        record;
+    dataJson jsonb := '[]';
+BEGIN
+
+    for r in SELECT re.id,
+                    re.lot_id,
+                    re.doc_date,
+                    mpd.name   as "organ_name",
+                    re.sumlot as "sum_lot",
+                    re.month,
+                    ra.state,
+                    re.pltf
+             FROM request_etp re
+                      INNER JOIN response_auction ra
+                                 ON re.lot_id = ra.lot_id
+                      LEFT JOIN man_organizations$payload$data mpd
+                                ON mpd.organ = re.organ
+             WHERE (re.lot_id = v_lot_id OR v_lot_id = 9223372036854775807)
+               AND (re.doc_date BETWEEN v_from_doc_date AND v_to_doc_date)
+               AND (mpd.name = v_organ_name OR v_organ_name = 'ALL')
+               AND (re.sumlot BETWEEN v_from_sum_lot AND v_to_sum_lot)
+               AND (re.month = v_month OR v_month = 2147483647)
+               AND (ra.state = v_state OR v_state = 2147483647)
+               AND (re.pltf = v_pltf)
+        loop
+            dataJson = dataJson ||  jsonb_agg(r);
+        end loop;
+    return dataJson::text;
+END;
+$$;
