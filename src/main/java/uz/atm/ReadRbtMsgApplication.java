900c6e3983d276f01847f0436d309a7e5c7cc48f
Shoxruh0912 <bekpulatovshoxruh9@gmail.com>
2022-07-13 10:52:58 +0500

    security changes added

--- src/main/java/uz/atm/ReadRbtMsgApplication.java
+++ src/main/java/uz/atm/ReadRbtMsgApplication.java
@@ -1,31 +1,48 @@
 package uz.atm;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import uz.atm.dto.auth.AuthUserCreateDto;
+import uz.atm.enums.Role;
 import uz.atm.properties.OpenApiProperties;
+import uz.atm.services.auth.AuthUserService;
 
 @SpringBootApplication
 @OpenAPIDefinition
 @EnableConfigurationProperties(value = OpenApiProperties.class)
 public class ReadRbtMsgApplication {
 
+    private final AuthUserService service;
+
+    public ReadRbtMsgApplication(AuthUserService service) {
+        this.service = service;
+    }
+
 
     public static void main(String[] args) {
         SpringApplication.run(ReadRbtMsgApplication.class, args);
     }
 
-/*    @Bean
-    static CommandLineRunner runner(final AuthUserService service) {
-        return args -> {
-            service.create(AuthUserCreateDto.builder()
-                    .username("admin")
-                    .password("admin")
-                    .role(Role.ADMIN)
-                    .build());
+    @Bean
+    public void run() throws Exception {
+        CommandLineRunner runner = (a) -> {
+            try {
+                service.create(AuthUserCreateDto.builder()
+                        .username("admin")
+                        .password("admin")
+                        .role(Role.ADMIN)
+                        .build());
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         };
-    }*/
+        runner.run("s", "b"
+        );
+    }
 
 
 }
